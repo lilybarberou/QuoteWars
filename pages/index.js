a7b864c53ebad41aac75cd1e29f60579f0f7e357
Lily <hayashi.quan@gmail.com>
2022-05-06 09:25:06 +0200

    perf: add meta tags in head

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,32 @@ export default function Home() {
   return (
     <>
       <Head>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="theme-color" content="#4f46e5"/>
+
+        <meta name="keywords" content="quotewars, serie api, movie api, quote wars api, nahay" />
+        <meta name= "author" content= "Lily B" />
+
+        <meta property="og:title" content="Quote Wars - series & movie quotes API"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Quote Wars is a free API serving serie & movie quotes!"/>
+        <meta property="og:site_name" content="Quote Wars API"/>
+        <meta property="og:image" content="https://raw.githubusercontent.com/QuoteWars/main/public/static/images/banner.png"/>
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="590" />
+        <meta property="og:image:height" content="300" />
+        <meta property="og:url" content="https://quotewars.vercel.app"/>
+        
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image:alt" content="https://raw.githubusercontent.com/QuoteWars/main/public/static/images/banner.png"/>
+
+        <meta name="google-site-verification" content="ZFCVBtEpUhi4LdjJNKBjTqDLlOHtNWqSdHiF45NBKow" />
+
         <title>Quote Wars API</title>
-        <meta name="description" content="An Api to listen to your best mates!" />
+        <meta name="description" content="Quote Wars is a free API serving quality serie & movie quotes!" />
         <link rel="icon" href="/icon.ico" />
       </Head>
 
