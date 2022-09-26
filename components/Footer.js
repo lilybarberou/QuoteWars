1ea067f01fe87f44460a3a15398a16524086a474
Lily <hayashi.quan@gmail.com>
2022-09-26 19:35:13 +0200

    fix - add rel noreferrer + update portfolio link

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,16 @@
 export default function Footer() {
-    return(
+    return (
         <>
             <div className="container relative mx-auto max-w-4xl mb-5 mt-40 text-center">
-                <p>Created by {/* */}
-                    <a className='text-indigo-600 underline' href="https://lilyb.netlify.app" target="_blank">Lily B</a> 🌊 © 2022
+                <p>
+                    Created by {/* */}
+                    <a className="text-indigo-600 underline" href="https://www.lilybarberou.fr" target="_blank" rel="noreferrer">
+                        Lily B
+                    </a>{' '}
+                    🌊 © 2022
                 </p>
             </div>
             <div className="bg-gradient-to-r from-indigo-500 to-sky-500 h-2 w-screen"></div>
-        </>  
-    )
+        </>
+    );
 }
