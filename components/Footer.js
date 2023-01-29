6dd00e53741b7eebe51d221481575ef4afed1625
Lily <hayashi.quan@gmail.com>
2023-01-29 14:07:51 +0100

    fix - portfolio url

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,16 @@
 export default function Footer() {
     return (
         <>
-            <div className="container relative mx-auto max-w-4xl mb-5 mt-40 text-center">
+            <div className='container relative mx-auto max-w-4xl mb-5 mt-40 text-center'>
                 <p>
                     Created by {/* */}
-                    <a className="text-indigo-600 underline" href="https://www.lilybarberou.fr" target="_blank" rel="noreferrer">
+                    <a className='text-indigo-600 underline' href='https://lilybarberou.fr' target='_blank' rel='noreferrer'>
                         Lily B
                     </a>{' '}
                     🌊 © 2022
                 </p>
             </div>
-            <div className="bg-gradient-to-r from-indigo-500 to-sky-500 h-2 w-screen"></div>
+            <div className='bg-gradient-to-r from-indigo-500 to-sky-500 h-2 w-screen'></div>
         </>
     );
 }
